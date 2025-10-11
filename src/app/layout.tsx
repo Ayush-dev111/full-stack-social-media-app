897b97e73a0bc758d6c705e8fe83a6a583b09ace
Ayush-dev111 <ayushnegi4d6f@gmail.com>
2025-10-11 20:37:24 +0530

    code refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
-import Siderbar from "@/components/Siderbar";
+import Siderbar from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
